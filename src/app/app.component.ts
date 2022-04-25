bb6032133b4fca4cb8c99ea798d3baf429b12f86
Dvs <satyasri.125110044@gmail.com>
2022-04-25 18:34:59 +0530

    implemented table in quill editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Project';
+  text={
+    readonly:false,
+    value:'This is sample'
+  };
+
+  constructor(private primengConfig: PrimeNGConfig) {
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
+// primeng Imports
+import {CalendarModule} from 'primeng/calendar';
+import {AccordionModule} from 'primeng/accordion';
+import {CardModule} from 'primeng/card';
+import {TableModule} from 'primeng/table';
+
+
+
+import { EditorComponent } from './editor/editor.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AccordionModule,
+    CardModule,
+    CalendarModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
